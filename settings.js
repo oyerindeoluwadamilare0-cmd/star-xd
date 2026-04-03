0b7d1424ebcf314d1184c3b58c719e55feb89330
oyerindeoluwadamilare0-cmd <oyerindeoluwadamilare0@gmail.com>
2026-04-03 09:13:10 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@
 export default {
   // PUT YOUR SESSION ID BELOW (Required)
   // Get from: https://star.malvintech.sbs/
-  SESSION_ID: "",
+  SESSION_ID: "king~H4sIAAAAAAAAA5VUy46jRhT9lai2tsYYsMGWWgrGD7ANBtv4Fc2igAJjXmUog/GopUSTRaJopE42SRYtRfMHo6jzE/MVni/oT4hwd88kUjLpsCpule49955z7isQxV6KRqgA7VcAJ14GCSqPpMAItEHn4DgoAVVgQwJBG/hjSVxBxu4a86llFeu4kbsNY6KaabEQNo1Wfjy29mw6oQbrK3BdBfhgBp71mYRwnhNmgBV34urpzoRL6Bg1UdBQgis0gn060S0sR0xeV67AdZkReokXuT28RSFKYDBChQa95HnwZSXcT8XV3NxPKx1ljt1Mcm0xjBjnFNpyPsPrKRuOCTYl/3nwtfq42cFcNzcpTim63mbc8sfrDbE32jbBNGPuK91UbkHNpR7gp54bIVu2UUQ8Ujx77oKiJ6uCouuSuV53neCkKBoXqBKvLLs4mqiBbHXigsGOLzwPODMf9SXDh/3jXFaUSobWw1j1uUzl+zpTW9SmQT/UdWlP5n8DriVPWvH/z9yhqBmK3a+NbEqq2NtM6aFigEmrGA3hsb7sCBuZi3erYO+zz4N/EDXNP+EjF4Q6QraajQsxUldbjltUjNage1K7vUyE+3zLf4IPySH5HEpVlyzLb8Squs9tduX5dG3VCiOR53bS0vLGWbbtbV3Hcyp2rPJqrm+G+rqn7nxhasxnnOn0TWPP7qYt6mS31jS/sEVWFK4uHfmokG3Qrl9XQYJcLyUJJF4clTGqCqCdzZCVIHIZLtBHBdSU7W5BjdOOxAmDRndtxCFpTvDpMFxOaw1zikZh2BobV6AKcBJbKE2RLXkpiZNCQWkKXZSC9lcvqyBCR/JAW1mMoavA8ZKUGNEBBzG0nzh9uoSWFR8iMisiSywPKHlA+BhGhHiRm5ZTPEQwsbZehsQtJCloOzBI0cf+UIJs0CbJAX30rBjb5dhpY92ZCtQAVEF4ocOzyyjD8nW2xdMNimPbdPPL9EVe5oUYv4gQAVUQPLyjGKrJt6gmXaeoZvmwjFdBBMtk4MPXP7z//f725sf725tf7m9vXt/f3nx7f3vz5sM3796//eJ89+Z899P57vX57ufz3ffnu+++ON+9fv/2/MdvH359VxL12GiJy0YEekEK2kBUZ8VOycXeaJxkFjsYCD1XEF0BfBrMk74eGKSCMXMK5vqWnucxl3Ees25urN1pUMs3fHeWpouuzomnDenEV/+QBLTBuNd0olRDK0isY+2U7RV3YCw5ybSjSfM4mDlQ7BrLNF963qGSx01f5LYFw6pyNBtP2D6uGO5Brevhglo5w2U2lTqtkZhfldVslHkW+muxjRCJWcyrS4ahhmJvGsdKwkeENbGL4EwZD0+jPa5kKc4Wlb6pRQeRlZUOmZzg3Ol6bo/X/UnfR83jfAHX9bQx2tCS8Kj8i/OCx43nXVRZUl7+Oh66LJBH7v5bAg/IL6a5rv4lyeNO+hdfd+zhrMU3DEq2fX404SosoSfhSuaPGT9wBCoZ93Vqkqe0qUNwff2yCnAAiRMnYbm6IjuJLwpL4kOpfTly4s8UEwVfFnJXLFsPYEqET36aeyFKCQwxaNc5rkFTDNdoPrzSkhhLMN2WmzkM6rtZaY5CwHhGIHmyJxDKT6ub4PpPTr+XILgHAAA=",
 
   // PUT YOUR REMOVE BACKGROUND KEY (Optional)
   // Get from: https://remove.bg/api
@@ -45,7 +45,7 @@ export default {
   BOT_NAME: "sᴛᴀʀ-xᴅ",
 
   // Add Owner WhatsApp Number (without +)
-  OWNER_NUMBER: "263725787000",
+  OWNER_NUMBER: "2348149825074",
 
   // Command Mode: "public" or "private"
   // private = only owner can use commands
